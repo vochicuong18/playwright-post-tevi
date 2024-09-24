d7d821a804ae3b879f6e34a0d936aca974b34232
cuong.vo <cuong.vo@apac.merkleinc.com>
2024-09-24 16:23:57 +0700

    fixtures

--- ricoh/sanity/CheckOutAsUserTest_Fixtures.spec.js
+++ ricoh/sanity/CheckOutAsUserTest_Fixtures.spec.js
@@ -8,60 +8,60 @@ test('Checkout as user', async ({ headerPage, loginPage, myAccountPage, productL
         // await myAccountPage.checkContactInfo(customer)
     })
 
-    // await test.step('Check cart empty', async () => {
-    //     await headerPage.viewMiniCart()
-    //     if (!await headerPage.isEmptyCartTitleDisplayed()) {
-    //         await headerPage.viewShoppingCart()
-    //         await shoppingCartPage.empty()
-    //     }
-    // })
-    //
-    // await test.step('Add product to cart', async () => {
-    //     await headerPage.searchProduct(simpleProduct)
-    //     await productListPage.goToProductDetails(simpleProduct)
-    //     await productDetailsPage.addToCart(simpleProduct)
-    //
-    //     await headerPage.searchProduct(bundleProduct)
-    //     await productListPage.goToProductDetails(bundleProduct)
-    //     await productDetailsPage.addToCart(bundleProduct)
-    // })
-    //
-    // await test.step('Check shopping cart', async () => {
-    //     await headerPage.viewMiniCart()
-    //     await headerPage.viewShoppingCart()
-    //     await shoppingCartPage.checkProduct(simpleProduct)
-    //     await shoppingCartPage.checkProduct(bundleProduct)
-    //     await shoppingCartPage.checkSubTotal(subTotal)
-    //     await shoppingCartPage.checkGrandTotal(grandTotal)
-    // })
-    //
-    // await test.step('Shipping', async () => {
-    //     await shoppingCartPage.goToShippingPage()
-    //     await shippingPage.selectShippingMethod(bestWay.code)
-    // })
-    //
-    // await test.step('Payment', async () => {
-    //     await shippingPage.gotoCheckOutPage()
-    //     await checkoutPage.checkSubTotal(subTotal)
-    //     await checkoutPage.checkShippingFree(shippingFee)
-    //     await checkoutPage.checkGrandTotal(grandTotal)
-    //     await checkoutPage.checkShippingAddress(customer)
-    //     await checkoutPage.checkBillingAddress(customer)
-    //     await checkoutPage.selectPaymentMethod(cod.code)
-    //     await checkoutPage.agreeTerm()
-    //     await checkoutPage.placeOrder()
-    // })
-    //
-    // await test.step('Check order details', async () => {
-    //     await successPage.goToOrderDetailPage()
-    //     await orderDetailsPage.checkProduct(simpleProduct)
-    //     await orderDetailsPage.checkProduct(bundleProduct)
-    //     await orderDetailsPage.checkSubtotal(subTotal)
-    //     await orderDetailsPage.checkShippingFee(shippingFee)
-    //     await orderDetailsPage.checkGrandTotal(grandTotal)
-    //     await orderDetailsPage.checkShippingAddress(customer)
-    //     await orderDetailsPage.checkBillingAddress(customer)
-    //     await orderDetailsPage.checkShippingMethod(bestWay.string)
-    //     await orderDetailsPage.checkPaymentMethod(cod.string)
-    // })
+    await test.step('Check cart empty', async () => {
+        await headerPage.viewMiniCart()
+        if (!await headerPage.isEmptyCartTitleDisplayed()) {
+            await headerPage.viewShoppingCart()
+            await shoppingCartPage.empty()
+        }
+    })
+
+    await test.step('Add product to cart', async () => {
+        await headerPage.searchProduct(simpleProduct)
+        await productListPage.goToProductDetails(simpleProduct)
+        await productDetailsPage.addToCart(simpleProduct)
+
+        await headerPage.searchProduct(bundleProduct)
+        await productListPage.goToProductDetails(bundleProduct)
+        await productDetailsPage.addToCart(bundleProduct)
+    })
+
+    await test.step('Check shopping cart', async () => {
+        await headerPage.viewMiniCart()
+        await headerPage.viewShoppingCart()
+        await shoppingCartPage.checkProduct(simpleProduct)
+        await shoppingCartPage.checkProduct(bundleProduct)
+        await shoppingCartPage.checkSubTotal(subTotal)
+        await shoppingCartPage.checkGrandTotal(grandTotal)
+    })
+
+    await test.step('Shipping', async () => {
+        await shoppingCartPage.goToShippingPage()
+        await shippingPage.selectShippingMethod(bestWay.code)
+    })
+
+    await test.step('Payment', async () => {
+        await shippingPage.gotoCheckOutPage()
+        await checkoutPage.checkSubTotal(subTotal)
+        await checkoutPage.checkShippingFree(shippingFee)
+        await checkoutPage.checkGrandTotal(grandTotal)
+        await checkoutPage.checkShippingAddress(customer)
+        await checkoutPage.checkBillingAddress(customer)
+        await checkoutPage.selectPaymentMethod(cod.code)
+        await checkoutPage.agreeTerm()
+        await checkoutPage.placeOrder()
+    })
+
+    await test.step('Check order details', async () => {
+        await successPage.goToOrderDetailPage()
+        await orderDetailsPage.checkProduct(simpleProduct)
+        await orderDetailsPage.checkProduct(bundleProduct)
+        await orderDetailsPage.checkSubtotal(subTotal)
+        await orderDetailsPage.checkShippingFee(shippingFee)
+        await orderDetailsPage.checkGrandTotal(grandTotal)
+        await orderDetailsPage.checkShippingAddress(customer)
+        await orderDetailsPage.checkBillingAddress(customer)
+        await orderDetailsPage.checkShippingMethod(bestWay.string)
+        await orderDetailsPage.checkPaymentMethod(cod.string)
+    })
 })
